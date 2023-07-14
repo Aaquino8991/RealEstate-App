4f7bb915ae36b8f6944111e20ac5a9ccb4f74316
Anthony <aquinoanthony851@gmail.com>
2023-07-14 12:24:48 -0600

    Declaring variables, adding fetch

--- RealEstate-App.js
+++ RealEstate-App.js
@@ -1,3 +1,12 @@
 const myKey = config.MY_KEY;
 const host = config.HOST;
 
+const url = 'https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=new%20york&limit=10';
+
+fetch(url, {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': myKey,
+    'X-RapidAPi-Host': host
+  }
+});
