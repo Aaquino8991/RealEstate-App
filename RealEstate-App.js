3a2dff0f32f85e20927aff405b1d52977212210f
Anthony <aquinoanthony851@gmail.com>
2023-07-15 22:11:11 -0600

    Adding comments for functions

--- RealEstate-App.js
+++ RealEstate-App.js
@@ -1,31 +1,29 @@
-// const myKey = config.MY_KEY;
-// const host = config.HOST;
-
-// const url = 'https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=new%20york&limit=10';
-
-// function homeList(data) {
-//   data.forEach(city => {
-//     const element = document.createElement('div');
-//     element.textContent = city;
-    
-//   })
-// }
-
 document.addEventListener('DOMContentLoaded', () => {
-  const list = document.getElementById('house-list')
+  
+  const myKey = config.MY_KEY;
+  const host = config.HOST;
 
-  console.log(list)
-})
+  const url = 'https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=new%20york&limit=10';
 
+  // Create elements for each city
+  function homeList(data) {
+    data.forEach(city => {
+      const element = document.createElement('div');
+      element.textContent = city;
+      document.getElementById('house-list');
+    })
+  }
+  
+  // Fetch data from API
+  fetch(url, {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': myKey,
+      'X-RapidAPi-Host': host
+    }
+  })
 
-// fetch(url, {
-//   method: 'GET',
-//   headers: {
-//     'X-RapidAPI-Key': myKey,
-//     'X-RapidAPi-Host': host
-//   }
-// })
-
-// .then (res => res.json())
-// .then (data => console.log(data))
-// .catch(error => console.error(error));
+  .then (res => res.json())
+  .then (data => console.log(data))
+  .catch(error => console.error(error));
+});
