6531342401bb5a50cf5b244066f36ce75bf21160
Anthony <aquinoanthony851@gmail.com>
2023-07-15 21:55:37 -0600

    Grabbing the house-list id

--- RealEstate-App.js
+++ RealEstate-App.js
@@ -1,20 +1,27 @@
-const myKey = config.MY_KEY;
-const host = config.HOST;
+// const myKey = config.MY_KEY;
+// const host = config.HOST;
 
-const url = 'https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=new%20york&limit=10';
+// const url = 'https://realty-in-us.p.rapidapi.com/locations/v2/auto-complete?input=new%20york&limit=10';
 
-function homeList(data) {
-  
-}
+// function homeList(data) {
+//   data.forEach(city => {
+//     const element = document.createElement('div');
+//     element.textContent = city;
+    
+//   })
+// }
+const list = document.querySelector('#wrapper')
 
-fetch(url, {
-  method: 'GET',
-  headers: {
-    'X-RapidAPI-Key': myKey,
-    'X-RapidAPi-Host': host
-  }
-})
+console.log(list)
 
-.then (res => res.json())
-.then (data => console.log(data))
-.catch(error => console.error(error));
+// fetch(url, {
+//   method: 'GET',
+//   headers: {
+//     'X-RapidAPI-Key': myKey,
+//     'X-RapidAPi-Host': host
+//   }
+// })
+
+// .then (res => res.json())
+// .then (data => console.log(data))
+// .catch(error => console.error(error));
